e933d84dc1ad43b14f1739ce082e4731a486ddbb
brice <bricerabouin@hotmail.com>
2024-06-17 19:47:29 +0200

    add dots header

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -4,8 +4,9 @@ export const Header = () => {
         <div className="text-center flex justify-between items-center mt-2">
             {/* <h1 className="text-center text-3xl font-bold mt-2 mb-2 text-emerald-400">M:É</h1> */}
             <img src='/MOI.svg' className='w-20' alt="logo" />
-            <div className='flex gap-4'>
-            </div>
+            <button className="py-1 px-3 rounded-md shadow-[0_8px_30px_rgb(0,0,0,0.12)]">
+                <img className='w-8 h-8 ' src='/dots.svg' alt="plus" />
+            </button>
         </div>
     );
 };
