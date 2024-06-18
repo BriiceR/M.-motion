422eb29dce80d61dd3464056a3844f2895a38178
brice <bricerabouin@hotmail.com>
2024-06-19 00:02:55 +0200

    add name of tabs

--- src/components/tabBar.tsx
+++ src/components/tabBar.tsx
@@ -6,10 +6,10 @@ const TabBar = () => {
     const [activeIndex, setActiveIndex] = useState(0);
 
     const tabs = [
-        { to: '/app/list', icon: '/list.svg', alt: 'list' },
-        { to: '/app/chart', icon: '/chart.svg', alt: 'chart' },
-        { to: '/app/dashboard', icon: '/emoji_4.svg', alt: 'dashboard' },
-        { to: '/app/idea', icon: '/idea.svg', alt: 'idea' },
+        { to: '/app/list', icon: '/list.svg', alt: 'liste' },
+        { to: '/app/chart', icon: '/chart.svg', alt: 'stat' },
+        { to: '/app/dashboard', icon: '/emoji_4.svg', alt: '' },
+        { to: '/app/idea', icon: '/idea.svg', alt: 'idée' },
         { to: '/app/profil', icon: '/profil.svg', alt: 'profil' },
     ];
 
@@ -20,26 +20,29 @@ const TabBar = () => {
 
     return (
         <div className="fixed bottom-0 left-0 right-0 bg-white border-t border-gray-200 shadow-md">
-            <div className="relative flex justify-around p-4 mb-4">
+            <div className="relative flex justify-around px-4 pt-4 mb-4">
                 <div
-                    className="absolute top-[-0px] h-1 bg-orange-300 transition-all duration-300"
+                    className="absolute top-0 h-1 bg-orange-300 transition-all duration-300"
                     style={{ left: `${(100 / tabs.length) * activeIndex}%`, width: `${100 / tabs.length}%` }}
                 ></div>
                 {tabs.map((tab, index) => (
                     <NavLink
                         key={index}
                         to={tab.to}
-                        className={`flex items-center relative ${location.pathname === tab.to ? 'text-blue-500' : 'text-gray-600 hover:text-blue-500'
+                        className={`flex flex-col items-center relative ${location.pathname === tab.to ? 'text-orange-300' : 'text-gray-600'
                             }`}
                     >
-                        <img
-                            src={tab.icon}
-                            alt={tab.alt}
-                            className={`w-8 h-8 ${tab.to === '/app/dashboard'
-                                ? 'bg-white rounded-full'
-                                : ''
-                                }`}
-                        />
+                        <div className="flex flex-col items-center">
+                            <img
+                                src={tab.icon}
+                                alt={tab.alt}
+                                className={`w-8 h-8 ${tab.to === '/app/dashboard'
+                                    ? 'w-12 h-12 p-2 bg-orange-300/50 rounded-md'
+                                    : ''
+                                    }`}
+                            />
+                            <span className="text-xs mt-1">{tab.alt}</span>
+                        </div>
                     </NavLink>
                 ))}
             </div>
--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -4,7 +4,7 @@ export const Header = () => {
         <div className="text-center flex justify-between items-center mt-2">
             {/* <h1 className="text-center text-3xl font-bold mt-2 mb-2 text-emerald-400">M:É</h1> */}
             <img src='/MOI.svg' className='w-20' alt="logo" />
-            <button className="py-1 px-3 rounded-md shadow-[0_8px_30px_rgb(0,0,0,0.12)]">
+            <button className="py-1 px-3 ">
                 <img className='w-8 h-8 ' src='/dots.svg' alt="plus" />
             </button>
         </div>
