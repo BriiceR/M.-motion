21e046312e86a07ca7eb3bd3164de231328124d8
brice <bricerabouin@hotmail.com>
2024-06-20 23:56:43 +0200

    change header icon

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -3,7 +3,7 @@ export const Header = () => {
     return (
         <div className="text-center flex justify-between items-center mt-2">
             {/* <h1 className="text-center text-3xl font-bold mt-2 mb-2 text-emerald-400">M:É</h1> */}
-            <img src='/MOI.svg' className='w-20' alt="logo" />
+            <img src='/favicon.svg' className='w-12 h-12' alt="logo" />
             <button className="py-1 px-3 ">
                 <img className='w-8 h-8 ' src='/dots.svg' alt="plus" />
             </button>
