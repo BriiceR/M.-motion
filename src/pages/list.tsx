051f76dad22ff117897d8bdd9a29fcd115a724ef
brice <bricerabouin@hotmail.com>
2024-06-22 20:29:15 +0200

    fix loader list

--- src/pages/list.tsx
+++ src/pages/list.tsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from 'react';
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
-import { Loader } from "../components/loader";
 import { useNavigate } from 'react-router-dom';
 import Layout from '../components/ui/layout';
 import { useStore } from '../store/useStore';
@@ -54,28 +53,26 @@ export const List = () => {
 
     return (
         <Layout>
-            {loading ? (
-                <Loader />
-            ) : (
-                <div className="flex flex-col justify-center gap-4 pt-10 pb-24">
-                    {showingResults
-                        .sort((a: any, b: any) => new Date(b.time).getTime() - new Date(a.time).getTime())
-                        .map((data: any, index: number) => (
-                            <div key={index} className="p-4 rounded-md shadow-[0_8px_30px_rgb(0,0,0,0.12)] bg-zinc-50">
-                                <div className="flex justify-between items-center">
-                                    <img src={data.mood} className='w-12' alt="Selected Mood" />
-                                    {data.category &&
-                                        <p className={`${options.find(opt => opt.value === data.category)?.color} text-white py-1 px-2 rounded-md`}>{data.category}</p>
-                                    }
-                                    <p className="text-sm">{formatDate(data.time)}</p>
-                                </div>
 
-                                <p className="mt-2">{data.emotion}</p>
-                                <p>{data.description}</p>
+            <div className="flex flex-col justify-center gap-4 pt-10 pb-24">
+                {showingResults
+                    .sort((a: any, b: any) => new Date(b.time).getTime() - new Date(a.time).getTime())
+                    .map((data: any, index: number) => (
+                        <div key={index} className="p-4 rounded-md shadow-[0_8px_30px_rgb(0,0,0,0.12)] bg-zinc-50">
+                            <div className="flex justify-between items-center">
+                                <img src={data.mood} className='w-12' alt="Selected Mood" />
+                                {data.category &&
+                                    <p className={`${options.find(opt => opt.value === data.category)?.color} text-white py-1 px-2 rounded-md`}>{data.category}</p>
+                                }
+                                <p className="text-sm">{formatDate(data.time)}</p>
                             </div>
-                        ))}
-                </div>
-            )}
+
+                            <p className="mt-2">{data.emotion}</p>
+                            <p>{data.description}</p>
+                        </div>
+                    ))}
+            </div>
+
         </Layout>
     );
 };
