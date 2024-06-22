4e5830d26d371053f74f8459323b3ae2e0068492
brice <bricerabouin@hotmail.com>
2024-06-22 20:05:36 +0200

    add logo-loader

--- src/components/loader.tsx
+++ src/components/loader.tsx
@@ -1,11 +1,11 @@
-
+import Logo from '../assets/logo-loader.svg';
 
 
 export const Loader = () => {
 
     return (
         <div className="flex justify-center items-center h-screen bg-zinc-50">
-            <img className="w-16 h-16" src="/favicon.svg" alt="loader" />
+            <img className="" src={Logo} alt="loader" />
         </div >
     )
 }
