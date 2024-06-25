160c1a4a2b7e9d4c9a5e10ea174652455d1936ae
brice <bricerabouin@hotmail.com>
2024-06-25 22:46:09 +0200

    fix header

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -1,12 +1,14 @@
 
 export const Header = () => {
     return (
-        <div className="text-center flex justify-between items-center mt-2">
-            {/* <h1 className="text-center text-3xl font-bold mt-2 mb-2 text-emerald-400">M:É</h1> */}
-            <img src='/favicon.svg' className='w-12 h-12' alt="logo" />
-            <button className="py-1 px-3 ">
-                <img className='w-8 h-8 ' src='/dots.svg' alt="plus" />
-            </button>
+        <div className="w-full fixed bg-white">
+            <div className="text-center flex justify-between items-center mt-2 px-6 ">
+                <img src='/favicon.svg' className='w-12 h-12' alt="logo" />
+                <button className="py-1 px-3 ">
+                    <img className='w-8 h-8 ' src='/dots.svg' alt="plus" />
+                </button>
+            </div>
+            <hr className="" />
         </div>
     );
 };
--- src/components/ui/layout.tsx
+++ src/components/ui/layout.tsx
@@ -4,9 +4,10 @@ import { Header } from './header';
 const Layout = ({ children }: { children: React.ReactNode }) => {
     return (
         <div className="flex justify-center">
-            <div className="w-screen px-6 h-screen">
+            <div className="w-screen h-screen">
                 <Header />
-                <hr />
+
+
                 {children}
             </div>
         </div>
